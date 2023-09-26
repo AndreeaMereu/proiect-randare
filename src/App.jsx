c3ca1940e46a8e116a46525a0489e5fa3b3f0f38
valentin.cocorada <valentin.cocorada@metro.digital>
2023-09-26 11:55:35 +0300

    actualizare timp livrare

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,46 @@ function getColor(count) {
   }
 }
 
+function TipLivrare({ count, className }) {
+  if (count < 36) {
+    return <h2 className={className}>Standard</h2>;
+  }
+
+  if (count >= 10 && count < 24) {
+    return <h2 className={className}>Red Code</h2>;
+  }
+
+  if (count >= 1 && count < 6) {
+    return <h2 className={className}>Express Loco</h2>;
+  }
+}
+
+function IntervalLivrare () {
+  const [count, setCount] = useState(0);
+  const handleOnIncrementClick = () => {
+    setCount(count + 1);
+  };
+  const handleOnDecrementClick = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+  <h1>{count} Ore</h1>
+    <TipLivrare className={getColor(count)} count={count} />
+  <button onClick={handleOnDecrementClick}>-</button>
+  <button onClick={handleOnIncrementClick}>+</button>
+  </div>
+  );
+};
+
 function App () {
 return (
   <div>
   <LivrareCargus />
+  <IntervalLivrare />
+  <IntervalLivrare />
+  <IntervalLivrare />
 </div>
 );
 }
