d7a3228ba0e4a88d69a6d93696d96c8785812043
valentin.cocorada <valentin.cocorada@metro.digital>
2023-09-26 10:53:23 +0300

    adaugare clasa css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
-function App() {
-
+const LivrareCargus = () => {
+  return <h1>Cargus Expres - servicii curierat</h1>
+}
+const App = () => {
+return (
+  <div>
+  <LivrareCargus />
+</div>
+);
 }
 
 export default App
